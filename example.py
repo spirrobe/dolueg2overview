d31e03c8a4c0400fc6b9a7c9bab899cf2f363f92
spirrobe <spirrobe@users.noreply.github.com>
2020-08-25 18:09:33 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# """
+# Created on Tue Jul  3 10:09:18 2018
+#
+# @author: spirrobe
+# """
+
+
+if __name__ == '__main__':
+    from web.htmlfileprocessor import htmlfileprocessor
+    htmlfileprocessor('template.txt', 'current.html')
