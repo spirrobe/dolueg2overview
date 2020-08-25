bc071254f0eb55522a8d87d6482e726d0a122f66
spirrobe <spirrobe@users.noreply.github.com>
2020-08-25 18:26:24 +0200

    Create gettimezones.py

---
+++ sql/util/gettimezones.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def gettimezones(*args, **kwargs):
+    print('Please provide your own gettimezones function here')
+    return False
+
+if __name__ == '__main__':
+    print('Please provide your own gettimezones function here')
