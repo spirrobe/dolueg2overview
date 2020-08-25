800090fdfc1b4e768528690277d57f3a1a4ee17f
spirrobe <spirrobe@users.noreply.github.com>
2020-08-25 18:26:01 +0200

    empty getdata.py function

---
+++ sql/util/getdata.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def getdata(*args, **kwargs):
+    print('Please provide your own getdata function here')
+    return False, False
+
+if __name__ == '__main__':
+    print('Please provide your own getdata function here')
